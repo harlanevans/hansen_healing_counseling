cf54ffdf8b9fe290fccf08e270284583aad042ea
Harlan Evans <harlanevans5@gmail.com>
2019-09-23 18:41:54 -0600

    moved logic of modal toggle to mobile.js instead of navmobile

--- src/components/Mobile/Mobile.js
+++ src/components/Mobile/Mobile.js
@@ -1,7 +1,8 @@
 import React from "react";
 import NavMobile from "./NavMobile";
+import NavModal from "./NavModal";
 import LandingMobile from "./LandingMobile";
-import LandingText from './LandingText';
+import LandingText from "./LandingText";
 import AboutMobile from "./AboutMobile";
 import ContactMobile from "./ContactMobile";
 import ServicesMobile from "./ServicesMobile";
@@ -12,13 +13,20 @@ import { Fade } from "react-reveal";
 // import { WhiteBGGlobal } from './Styles';
 import ScrollToTop from "../../ScrollToTop";
 
-const Mobile = () => {
-  return (
-    // <Fade duration={2000}>
-    <div>
-      {/* <div className='fixed'> */}
-        <NavMobile />
-      {/* </div> */}
+class Mobile extends React.Component {
+    state = { showModal: false };
+
+  toggleModal = () => {
+    const { showModal } = this.state;
+    this.setState({ showModal: !showModal });
+    console.log("Toggled")
+  };
+
+  render() {
+    return (
+      <div>
+        <NavMobile toggleModal={this.toggleModal}/>
+        <NavModal showModal={this.state.showModal} toggleModal={this.toggleModal}/>
         <ScrollToTop>
           <div id="home">
             <LandingMobile />
@@ -43,9 +51,9 @@ const Mobile = () => {
           </div>
         </ScrollToTop>
         <FooterMobile />
-        </div>
-    // </Fade>
-  );
-};
+      </div>
+    );
+  }
+}
 
 export default Mobile;
