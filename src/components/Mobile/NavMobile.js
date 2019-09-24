2f948dafd46aae0a3a9a76384c9c60563dae365d
Harlan Evans <harlanevans5@gmail.com>
2019-09-23 18:17:43 -0600

    pot fix to nav mobile fix

--- src/components/Mobile/NavMobile.js
+++ src/components/Mobile/NavMobile.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Icon } from "semantic-ui-react";
 import { HashLink } from "react-router-hash-link";
 import NavModal from "./NavModal";
-import { Fade } from 'react-reveal';
+import { Fade } from "react-reveal";
 
 class NavMobile extends React.Component {
   state = { showModal: false };
@@ -18,21 +18,21 @@ class NavMobile extends React.Component {
     return (
       <div>
         <div className="main-nav-mobile">
-            <HashLink to="/#home" smooth>
-              <Icon name="leaf" className="leaf-icon" size="large" />
-            </HashLink>
-              <Icon
-                name="bars"
-                className="drop-down"
-                size="large"
-                onClick={this.toggleModal}
-                style={{ color: "white" }}
-              />
+          <HashLink to="/#home" smooth>
+            <Icon name="leaf" className="leaf-icon" size="large" />
+          </HashLink>
+          <Icon
+            name="bars"
+            className="drop-down"
+            size="large"
+            onClick={this.toggleModal}
+            style={{ color: "white" }}
+          />
         </div>
         <Fade duration={3000} delay={200}>
-          <div className={showModal ? 'fadeIn-modal' : 'fadeOut-modal'}>
-        <NavModal showModal={showModal} toggleModal={this.toggleModal} />
-        </div>
+          <div className={showModal ? "fadeIn-modal" : "fadeOut-modal"}>
+            <NavModal showModal={showModal} toggleModal={this.toggleModal} />
+          </div>
         </Fade>
       </div>
     );
